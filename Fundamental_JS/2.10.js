78648a29521ec8a4ccec5e00062ebf644cb1f31d
Anna Saushkina <saushkinanna@gmail.com>
2024-06-15 22:13:51 +0300

    2.16

---
+++ Fundamental_JS/2.10.js
@@ -0,0 +1,8 @@
+let numbers = [1, 2, 3, 4, 5, 6, 7];
+function mainCode(numbers) {
+    // numbers - это массив чисел
+    // напишите ваш код к заданию до оператора return
+
+    return numbers;
+}
+mainCode()
